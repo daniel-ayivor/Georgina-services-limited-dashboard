d8be3fd456fd64a2be7b5700d71934aed2f3c833
daniel-ayivor <danielkofiayivor23@gmail.com>
2025-11-08 11:07:37 +0000

    images  error fixes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://georgina-server-code.onrender.com';
-export const API_BASE_URL ="http://localhost:8003"
+export const API_BASE_URL = "https://georgina-server-code.onrender.com" 
